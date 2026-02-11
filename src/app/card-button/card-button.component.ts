97eddb1ed30f54856ecbdb3bd089a3c8b0d757ac
Felipe Santana Santos <fsantos@iportsolutions.com.br>
2026-02-11 13:46:09 -0300

    feat: Criando template

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  templateUrl: './card-button.component.html',
+  //templateUrl: './card-button.component.html',
+  //cria um template para renderizar na pagina
+  //obs: que seja curto
+  template: `
+    <div class="card-button">
+        Adquirir
+    </div>
+  `,
   styleUrls: ['./card-button.component.scss']
 })
 export class CardButtonComponent {
