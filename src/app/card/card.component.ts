467d02dfcaa49a702fc66db0722305f20d16b5b8
Felipe Santana Santos <fsantos@iportsolutions.com.br>
2026-02-25 10:28:08 -0300

    feat: Utilizando o encapsulation emulate

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -13,11 +13,15 @@ interface IInfos{
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  //encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 
 //encapsulation: ViewEncapsulation.None
 //remove a encapsulação dos estilos ou seja, os estilos vão estar global
+
+//encapsulation: ViewEncapsulation.Emulated
+//os estilos não ficam globais, porém estilos globais afetam os componentes
 
 export class CardComponent {
  
